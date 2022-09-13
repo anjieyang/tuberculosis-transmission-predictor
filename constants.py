c4a4f8da0dade5154c137c5fa106e4a2675e23ae
Zhiyi Dong <105244271+Zhiyi-Dong-6@users.noreply.github.com>
2022-09-13 01:09:51 -0400

    Delete constants.py

--- constants.py
+++
@@ -1,5 +0,0 @@
-PDF_PATH = 'Building_Number_Maps_2021 Gonzalo'
-PIVOT_PATH = 'pivot points'
-GEO_SEMI_DATA_PATH = 'semi_data_geo coordinates'
-GEO_COORDINATE_PATH = 'geo coordinates'
-PDF_Y_MAX = 3370
