a69893146ad78e679b687232ff74bce2b836ebfe
Zhiyi Dong <105244271+Zhiyi-Dong-6@users.noreply.github.com>
2022-09-13 01:12:38 -0400

    New codes for clustering and visualization

---
+++ common_modules.py
@@ -0,0 +1,96 @@
+from distutils.command.build import build
+import numpy as np
+import pandas as pd
+import math
+
+
+class Building:
+    def __init__(self, building_num, x, y, longitude, latitude, prefix = None, suffix = None, from_range = False, range = None, category = 'valid'):
+        '''
+        :param 
+        :param 
+        :param 
+        :return: 
+        '''
+        self.building_num = building_num
+        self.x = x
+        self.y = y
+        self.prefix = prefix
+        self.suffix = suffix
+        self.category = category
+        self.longitude = longitude
+        self.latitude = latitude
+        self.from_range = from_range
+        self.range = range
+        self.category = category
+        self.id = Building.xstr(prefix)+str(building_num)+Building.xstr(suffix)
+    # rewrite print, +, -, *, / functions
+    def __str__(self):
+        return "ID = {}| X,Y = ({},{})| Category is {}".format(self.id, self.x, self.y, self.category)   
+    def __sub__(self, point):
+        return Building(building_num=None, x=self.x-point.x, y=self.y-point.y, longitude=self.longitude-point.longitude, latitude=self.latitude-point.latitude)
+    def __add__(self, point):
+        return Building(building_num=None, x=self.x+point.x, y=self.y+point.y, longitude=self.longitude+point.longitude, latitude=self.latitude+point.latitude)
+    def __mul__(self, value):
+        return Building(building_num=None, x=self.x*value, y=self.y*value, longitude=self.longitude*value, latitude=self.latitude*value)
+    # def __truediv__ (self, value):
+    #     return Building(building_num=None, x=self.x/value, y=self.y/value, longitude=self.longitude/value, latitude=self.latitude/value)
+    
+    @staticmethod
+    def Euclidean_dist(point1, point2):
+        # the Euclidean distance between two points
+        sub = point1-point2
+        # return math.sqrt(pow(sub.longitude,2) + pow(sub.latitude, 2))
+        return math.sqrt(pow(sub.x,2) + pow(sub.y, 2))
+    def xstr(s):
+        return '' if s is None or s =='None' else str(s)
+   
+def get_data(read_path , map_name):
+    '''
+    read all values of all buildings
+    :param read_path: the path of target file
+    :param map_name: the name of target file
+    :return: a list of buildings, each building belongs to Building class
+    '''
+    
+    df = pd.read_excel(read_path + '/' + map_name)
+    data_size = len(df)
+    building_lst = []
+        
+    for i in range(data_size):
+        if df["Category"][i] == 'valid':
+            building_id = df["Building Number"][i]
+            x = df["X"][i]
+            y = df["Y"][i]
+            prefix = df["Prefix"][i]
+            suffix = df["Suffix"][i]
+            from_range = df["From a Range?"][i]
+            range_value = df["Range"][i]
+            longitude = df["Longitude"][i]
+            latitude = df["Latitude"][i]
+            
+            building_lst.append(Building(building_num=building_id, x=x, y=y, longitude=longitude, latitude=latitude, prefix = prefix, suffix = suffix, from_range = from_range, range = range_value))
+        
+        
+    return building_lst
+   
+def dist_matrix(building_lst):
+    '''
+    read all values of all buildings
+    :param building_lst: a list of buildings, each building belongs to Building class
+    :return: a numpy matrix, contains the distance of two buildings
+    '''
+    data_size = len(building_lst)
+    dist_matrix = np.zeros([data_size,data_size])
+    for i in range(data_size):
+        for j in range(data_size):
+            dist_matrix[i][j] = Building.Euclidean_dist(building_lst[i], building_lst[j])
+    return dist_matrix
+   
+   
+## testing
+READ_PATH = "Geo_coordinates"
+MAP = "Arctic_Bay.xls"
+if __name__ == "__main__":
+    building_lst = get_data(READ_PATH, MAP)
+    
---
+++ visualization.py
@@ -0,0 +1,84 @@
+from pdf_annotate import PdfAnnotator, Appearance, Location
+import matplotlib.pyplot as plt
+import numpy as np
+import colors, k_means, hierarchical
+import random
+import os
+
+# hyper-parameters
+READ_PATH = "Geo_coordinates"
+PDF_PATH = "Clustering"
+PDF_NAME = "Arctic_Bay_Building_No_2021_Wall.pdf"
+WRITE_PATH = "Clustering"
+# MAPS = os.listdir(READ_PATH)
+MAP = "Arctic_Bay.xls"
+CENTERS_NUM = 20
+
+class Cluster:
+    def __init__(self, cluster_id, center, buildings_lst):
+        self.cluster_id = cluster_id
+        self.buildings_lst = buildings_lst
+        self.cluster_size = len(buildings_lst)
+        self.center = center
+    def coloring(self, annotator, color, size):
+        for building in self.buildings_lst:
+            x = building.x
+            y = building.y
+            id = building.id
+            x1 = x - size
+            x2 = x + size
+            y1 = 3370 - y - size
+            y2 = 3370 - y + size
+            annotator.add_annotation(annotation_type='square', location=Location(x1=x1, y1=y1, x2=x2, y2=y2, page=0), appearance=Appearance(fill=color, stroke_width=0))
+            annotator.add_annotation(annotation_type='text', location=Location(x1=x1, y1=y1, x2=x2, y2=y2, page=0), appearance=Appearance(content=id, fill=(0,0,0), font_size=5)) # black text
+            # print(x1,y1)
+    def bounds(self, annotator):
+        pass
+         
+    
+def get_Clusters_kmean(read_path, map,k=CENTERS_NUM):
+    clustering = k_means.clustering(read_path, map, k)
+    clusters_lst = []
+    for i in range(k):
+        center = clustering.center_lst[i]
+        groups = clustering.groups[i]
+        cluster = Cluster(i, center, groups)
+        clusters_lst.append(cluster)
+    return clusters_lst
+
+
+def get_Clusters_hierarchical(read_path, map,k=CENTERS_NUM):
+    clustering = hierarchical.clustering(read_path, map, k=CENTERS_NUM)
+    clusters_lst = []
+    for i in range(k):
+        groups = clustering.clusters_lst[i].points
+        cluster = Cluster(i, None, groups)
+        clusters_lst.append(cluster)
+    return clusters_lst
+        
+
+if __name__ == "__main__":
+    COLORS = colors.COLORS
+    clusters_lst = get_Clusters_hierarchical(READ_PATH, MAP, k=CENTERS_NUM)
+        
+    # colors_index = np.random.choice(22, CENTERS_NUM, replace=False)
+    # colors_lst = [COLORS[i] for i in colors_index]
+    for i in range(CENTERS_NUM):
+        annotator = PdfAnnotator(PDF_PATH + '/' + PDF_NAME)
+        cluster = clusters_lst[i]
+        # color = [rgb/255 for rgb in colors_lst[i]]
+        # r = color[0]
+        # g = color[1]
+        # b = color[2]
+        r = random.random()
+        g = random.random()
+        b = random.random()
+        cluster.coloring(annotator, (r,g,b, 0.8) , size = 10)
+        annotator.write(WRITE_PATH+ "/" + PDF_NAME)
+    #     # cluster.bounds(annotator)
+    #     # annotator.write(WRITE_PATH+ "/" + PDF_NAME)
+            
+            
+            
+        
+    
